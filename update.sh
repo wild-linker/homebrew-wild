ef48a3492970acc8fce8fd93df9961a2a97d7d05
David Lattimore <dvdlttmr@gmail.com>
2026-05-05 14:51:02 +1000

    Add script to update formula

--- update.sh
+++ update.sh
@@ -1,20 +1,25 @@
+#!/bin/bash
+set -eu
+
+
+cat <<EOF > Formula/wild.rb
 # This file is automatically edited by ../update.sh
 class Wild < Formula
   desc "Fast linker for Linux"
   homepage "https://github.com/wild-linker/wild"
-  version "0.8.0"
+  version "$VERSION"
   license any_of: ["MIT", "Apache-2.0"]
   depends_on :linux
 
   on_linux do
     on_arm do
       url "https://github.com/wild-linker/wild/releases/download/#{version}/wild-linker-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "4de1727db12347c6fbb9fbf67a4b0f15e0344e6de8bb3c0143876d2464143861"
+      sha256 "$ARM_SHA"
     end
 
     on_intel do
       url "https://github.com/wild-linker/wild/releases/download/#{version}/wild-linker-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "44ed25fff3657ca62c25a441e60861d4a4061b44c096b998699cc86fa596490b"
+      sha256 "$X86_SHA"
     end
   end
 
@@ -27,3 +32,4 @@ class Wild < Formula
     assert_match version.to_s, shell_output("#{bin}/wild --version")
   end
 end
+EOF
